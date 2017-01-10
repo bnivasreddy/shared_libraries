cbf704866cfc9be6bded78eee5d41bd0ccdd22c5
Pipeline Author <nfjsuser1@gmail.com>
2017-01-09 23:59:06 -0500

    updates

--- vars/sReports.groovy
+++ vars/sReports.groovy
@@ -72,5 +72,9 @@ def call(body) {
 		results.each { rkey, rvalue ->
 			println "$rvalue"
 		}
+manager.addWarningBadge("There were ${results.size()} quality metric violations")
+	manager.addShortText("fail",'white','red', '1px', 'gray')
+	summary = manager.createSummary("warning.gif")
+	manager.buildFailure()
 	}
 }
