eb51ab79c30e44e6bf03c31eb11ee9c60395fbf1
Pipeline Author <nfjsuser1@gmail.com>
2017-01-11 00:46:18 -0500

    splitting code into subroutines 11

--- vars/sReports.groovy
+++ vars/sReports.groovy
@@ -23,14 +23,17 @@ def call(body) {
 
 	metricsParam = metricDefinitions.keySet().join(',')
 	sonarUrl2 = "${sonarUrl}/api/resources?resource=${sonarProjectId}&format=xml&metrics=${metricsParam}"
-	println "sonarUrl2 = $sonarUrl2"
+	// println "sonarUrl2 = $sonarUrl2"
 	sonarXml = sonarUrl2.toURL().text
-	println "sonarXml = $sonarXml"
+	// println "sonarXml = $sonarXml"
         def resources = new XmlSlurper().parseText(sonarXml)
 	resources.resource.msr.each { msr ->
   		sonarMetrics[msr.key.text()] = msr.val.text()
-		println "A"
+		// println "A"
   	}
+
+	mParam2 = sonarMetrics.keySet().join(',');
+	println "mParam2 = $mParam2"
 	// resources = null;
 
 	float sonarVal
