91014f2aeadf119f753dcaca552a6c30d3aea257
Pipeline Author <nfjsuser1@gmail.com>
2017-01-10 23:00:34 -0500

    splitting code into subroutines 4

--- vars/sReports.groovy
+++ vars/sReports.groovy
@@ -1,74 +1,50 @@
 
-
-def getSonarData(sonarProjectId, sonarUrl, metricsToCheck) {
-	def sonarData = [:]
-
-
-	
-	metricsParam = metricsToCheck.keySet().join(',')
-	sonarUrl = "${sonarUrl}/api/resources?resource=${sonarProjectId}&format=xml&metrics=${metricsParam}"
-	sonarXml = sonarUrl.toURL().text
-        def resources = new XmlParser().parseText(sonarXml)
-	println sonarXml
-	resources.resource[0].msr.findAll
-	{it}.each { msr ->
-		sonarData[msr.key.text()] = msr.val.text()
-	}
-	//resources = null;
-	if (sonarData) {	
-		sonarData.each { rkey, rvalue ->
-	   		println "$rkey $rvalue"
-		}
-	}
-
-
-
-
-	return sonarData
-
-}
-
-
 def call(body) {
 
-
-
+   // add the code to make this easily "callable" from the pipeline code 
+   body.resolveStrategy = Closure.DELEGATE_FIRST
+   body.delegate = jenkinsValues
+   body()
 
 
    // define the types of limits we have
-
    HIGH_LIMIT_DEF = 'HIGH'
    LOW_LIMIT_DEF = 'LOW'
 
    // define the set of metrics that we're interested in 
    metricsToCheck = [
-					'violations':[desc:'Total Violations', jenkinsRef:'violationsThreshold', limitType:HIGH_LIMIT_DEF],
-					'blocker_violations':[desc:'Blocker Violations', jenkinsRef:'blockerThreshold', limitType:HIGH_LIMIT_DEF],
-					'critical_violations':[desc:'Critical Violations', jenkinsRef:'criticalThreshold', limitType:HIGH_LIMIT_DEF],
-					'major_violations':[desc:'Major Violations', jenkinsRef:'majorThreshold', limitType:HIGH_LIMIT_DEF],
-					]
+	'violations':[desc:'Total Violations', jenkinsRef:'violationsThreshold', limitType:HIGH_LIMIT_DEF],
+	'blocker_violations':[desc:'Blocker Violations', jenkinsRef:'blockerThreshold', limitType:HIGH_LIMIT_DEF],
+	'critical_violations':[desc:'Critical Violations', jenkinsRef:'criticalThreshold', limitType:HIGH_LIMIT_DEF],
+	'major_violations':[desc:'Major Violations', jenkinsRef:'majorThreshold', limitType:HIGH_LIMIT_DEF],
+	]
 
     // delcare a map to hold the values passed in from jenkins
-    def jenkinsValues = [:]
-    // add the code to make this easily "callable" from the pipeline code 
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = jenkinsValues
-    body()
+    def jenkinsValues = [:]    
 
     
     // declare a map to hold the sonar metrics we'll parse out of the xml
     def sonarMetrics = [:]
 
     // get the sonar data
-    // sonarMetrics = getSonarData(jenkinsValues.sonarProjectId,jenkinsValues.sonarUrl,metricsToCheck)
 
-sonarMetrics = getSonarData(jenkinsValues.sonarProjectId, jenkinsValues.sonarUrl, metricsToCheck)
+    metricsParam = metricsToCheck.keySet().join(',')
+    sonarUrl = "${sonarUrl}/api/resources?resource=${sonarProjectId}&format=xml&metrics=${metricsParam}"
+    sonarXml = sonarUrl.toURL().text
+    
+    def resources = new XmlParser().parseText(sonarXml)
+    println sonarXml
+    resources.resource[0].msr.findAll
+    {it}.each { msr ->
+	sonarData[msr.key.text()] = msr.val.text()
+     }
 
-if (sonarMetrics) {	
-		sonarMetrics.each { rkey, rvalue ->
-			println "$rkey, $rvalue"
+     if (sonarData) {	
+		sonarData.each { rkey, rvalue ->
+	   		println "$rkey $rvalue"
 		}
-}
+	}
+    
   
 
    //  do the math to see if we are over or under a limit as appropriate
