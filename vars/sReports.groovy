d6d1b947ec227091f13494f787763e8bcd7291b9
Pipeline Author <nfjsuser1@gmail.com>
2017-01-10 23:10:39 -0500

    splitting code into subroutines 4

--- vars/sReports.groovy
+++ vars/sReports.groovy
@@ -1,51 +1,52 @@
-
 def call(body) {
 
-  
    // define the types of limits we have
+
    HIGH_LIMIT_DEF = 'HIGH'
    LOW_LIMIT_DEF = 'LOW'
 
    // define the set of metrics that we're interested in 
    metricsToCheck = [
-	'violations':[desc:'Total Violations', jenkinsRef:'violationsThreshold', limitType:HIGH_LIMIT_DEF],
-	'blocker_violations':[desc:'Blocker Violations', jenkinsRef:'blockerThreshold', limitType:HIGH_LIMIT_DEF],
-	'critical_violations':[desc:'Critical Violations', jenkinsRef:'criticalThreshold', limitType:HIGH_LIMIT_DEF],
-	'major_violations':[desc:'Major Violations', jenkinsRef:'majorThreshold', limitType:HIGH_LIMIT_DEF],
-	]
+					'violations':[desc:'Total Violations', jenkinsRef:'violationsThreshold', limitType:HIGH_LIMIT_DEF],
+					'blocker_violations':[desc:'Blocker Violations', jenkinsRef:'blockerThreshold', limitType:HIGH_LIMIT_DEF],
+					'critical_violations':[desc:'Critical Violations', jenkinsRef:'criticalThreshold', limitType:HIGH_LIMIT_DEF],
+					'major_violations':[desc:'Major Violations', jenkinsRef:'majorThreshold', limitType:HIGH_LIMIT_DEF],
+					]
 
     // delcare a map to hold the values passed in from jenkins
-    def jenkinsValues = [:]    
-
-    
-    // declare a map to hold the sonar metrics we'll parse out of the xml
-    def sonarMetrics = [:]
-
+    def jenkinsValues = [:]
     // add the code to make this easily "callable" from the pipeline code 
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = jenkinsValues
     body()
 
-    // get the sonar data
+    // set a few basic values
+    prjName = jenkinsValues.projectName
+    sonarProjectId = jenkinsValues.sonarProjectId
+    sonarUrl = jenkinsValues.sonarUrl
 
-    metricsParam = metricsToCheck.keySet().join(',')
-    sonarUrl = "${jenkinsValues.sonarUrl}/api/resources?resource=${jenkinsValues.sonarProjectId}&format=xml&metrics=${metricsParam}"
+    // declare a map to hold the sonar metrics we'll parse out of the xml
+    def sonarMetrics = [:]
+
+    // construct the REST API call to get the xml
+    metricsList = metricsToCheck.keySet().join(',')
+    sonarUrl = "${sonarUrl}/api/resources?resource=${sonarProjectId}&format=xml&metrics=${metricsList}"
     sonarXml = sonarUrl.toURL().text
-    
+
+    //  parse the xml and fill in the map
     def resources = new XmlParser().parseText(sonarXml)
     println sonarXml
-    resources.resource[0].msr.findAll
-    {it}.each { msr ->
-	sonarMetrics[msr.key.text()] = msr.val.text()
-     }
+    resources.resource[0].msr.each { msr ->
+  	sonarMetrics[msr.key.text()] = msr.val.text()
+    }
 
-     if (sonarMetrics) {	
+    if (sonarMetrics) {	
 		sonarMetrics.each { rkey, rvalue ->
 	   		println "$rkey $rvalue"
 		}
 	}
-    
-  
+    // null this out so it doesn't cause a serialization issue back in the pipeline
+    resources = null;
 
    //  do the math to see if we are over or under a limit as appropriate
    float sonarVal
