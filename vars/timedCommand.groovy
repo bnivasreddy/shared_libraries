79a4a86863d85372efa699ae673a7bf7ba68b803
Pipeline Author <nfjsuser1@gmail.com>
2017-02-09 23:53:42 -0500

    adding timedCommand

---
+++ vars/timedCommand.groovy
@@ -0,0 +1,17 @@
+// vars/timedCommand.groovy
+
+def setCommand(command-to-run) { command = command-to-run }
+
+def getCommand() { command }
+
+def runCommand() {
+
+      timestamps {
+
+            commandOutput = sh( script: "${command}", returnStdout: true).trim()
+
+       }
+
+}
+
+def getCommandOutput() { commandOutput }
