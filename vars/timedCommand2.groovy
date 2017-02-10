65d0f4a90611338366542668bd3aaee9291346d4
Pipeline Author <nfjsuser1@gmail.com>
2017-02-10 01:54:25 -0500

    add timedCommand2

---
+++ vars/timedCommand2.groovy
@@ -0,0 +1,24 @@
+// vars/timedCommand2.groovy
+class timedCommand2 implements Serializable {
+
+    private String cmd
+    private String cmdOut
+
+    def setCommand(commandToRun) {
+       cmd = commandToRun
+    }
+
+    def getCommand() {
+       cmd
+    }
+
+    def runCommand() {
+       timestamps {
+          cmdOut = sh (script:"${cmd}", returnStdout:true).trim()
+       }
+    }
+
+    def getOutput() {
+       cmdOut
+    }
+}
