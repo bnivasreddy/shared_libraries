f9ba151bbaea034c59388652c92960ef2f7db885
Pipeline Author <nfjsuser1@gmail.com>
2017-02-11 12:59:00 -0500

    update

--- vars/timedCommand5.groovy
+++ vars/timedCommand5.groovy
@@ -3,6 +3,7 @@
 @Grab('org.apache.commons:commons-lang3:3.4+')
 import org.apache.commons.lang.time.StopWatch
 
+@NonCPS
 def call(Closure commands) { 
    node('worker') {
       def sw = new StopWatch()
