106982b97402b080d37010643390745c41e398c3
Pipeline Author <nfjsuser1@gmail.com>
2017-02-11 13:11:53 -0500

    update

--- vars/timedCommand5.groovy
+++ vars/timedCommand5.groovy
@@ -6,7 +6,7 @@ import org.apache.commons.lang.time.StopWatch
 @NonCPS
 def call(String cmdToRun) { 
       def sw = new StopWatch()
-      // echo "sw.getTime()"
+      echo "sw.getTime()"
       sh "${cmdToRun}"
       // echo "sw.getTime()"
       //delete sw
