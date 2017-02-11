ec4b70ee3f42497c23405ef6960fd74277b79662
Pipeline Author <nfjsuser1@gmail.com>
2017-02-11 17:24:31 -0500

    update

--- vars/timedCommand5.groovy
+++ vars/timedCommand5.groovy
@@ -3,17 +3,12 @@
 @Grab('org.apache.commons:commons-lang3:3.4+')
 import org.apache.commons.lang.time.StopWatch
 
-// @NonCPS
 def call(String cmdToRun) { 
       def sw = new StopWatch()
       def proc = "$cmdToRun".execute()
       sw.start()
-     
       proc.waitFor()
-      
       sw.stop()
-      
       println( "The process took ${(sw.getTime()/1000).toString()} seconds.\n")
-      sw = null
-   
+      // sw = null
 }
