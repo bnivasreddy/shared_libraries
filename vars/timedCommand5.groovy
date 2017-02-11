216b9474fdbb4250ad26712a0f7feb9a13c92835
Pipeline Author <nfjsuser1@gmail.com>
2017-02-11 12:51:58 -0500

    using grab:

---
+++ vars/timedCommand5.groovy
@@ -0,0 +1,13 @@
+// vars/timedCommand5
+
+@Grab('org.apache.commons:commons-lang3:3.4+')
+import org.apache.commons.lang.time.StopWatch
+
+def call(Closure commands) { 
+   node('worker') {
+      def sw = new StopWatch()
+      echo sw.getTime()
+      commands()
+      echo sw.getTime()
+   }
+}
