76195b5b6f5d6240ea808f0416e88c1939002806
Pipeline Author <nfjsuser1@gmail.com>
2017-02-11 12:54:23 -0500

    update

--- vars/timedCommand5.groovy
+++ vars/timedCommand5.groovy
@@ -6,8 +6,8 @@ import org.apache.commons.lang.time.StopWatch
 def call(Closure commands) { 
    node('worker') {
       def sw = new StopWatch()
-      echo sw.getTime()
+      echo "sw.getTime()"
       commands()
-      echo sw.getTime()
+      echo "sw.getTime()"
    }
 }
