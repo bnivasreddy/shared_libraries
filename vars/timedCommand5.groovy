7e99151f30ba7943d90b869fdfafa9a392fecfe2
Pipeline Author <nfjsuser1@gmail.com>
2017-02-11 16:20:50 -0500

    update

--- vars/timedCommand5.groovy
+++ vars/timedCommand5.groovy
@@ -8,7 +8,7 @@ def call(String cmdToRun) {
       def sw = new StopWatch()
       echo sw.getTime().toString()
       echo "${cmdToRun}"
-      cmdOutput = echo sh (script:"${cmd}", returnStdout:true).trim()
+      cmdOutput = echo sh (script:"${cmdToRun}", returnStdout:true).trim()
       echo "end time"
       echo sw.getTime().toString()
       // delete sw
