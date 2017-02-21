f13801f0c6e9885acb4f8020a798849232752005
Pipeline Author <nfjsuser1@gmail.com>
2017-02-21 13:31:03 -0500

    add new function

--- src/org/conf/Utilities.groovy
+++ src/org/conf/Utilities.groovy
@@ -3,5 +3,8 @@ class Utilities {
   static def gbuild(script, args) {
     script.sh "${script.tool 'gradle3.2'}/bin/gradle ${args}"
   }
+  static def getLastBuildStatus() {
+    return currentBuild.rawBuild.getPreviousSuccessfulBuild()
+  }
 }
 
