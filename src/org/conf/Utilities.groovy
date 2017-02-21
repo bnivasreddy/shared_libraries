85cbf5c29ab2b3127b5eb6a4b3d56e3b589ea2f5
Pipeline Author <nfjsuser1@gmail.com>
2017-02-21 13:49:56 -0500

    updated to pass script

--- src/org/conf/Utilities.groovy
+++ src/org/conf/Utilities.groovy
@@ -4,7 +4,7 @@ class Utilities {
     script.sh "${script.tool 'gradle3.2'}/bin/gradle ${args}"
   }
   static def getLastBuildStatus(script)
-    return script.currentBuild.rawBuild.getPreviousSuccessfulBuild()
+    script.currentBuild.rawBuild.getPreviousSuccessfulBuild()
   }
 }
 
